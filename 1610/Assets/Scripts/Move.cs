8d43620456b18c6bd9eaeec5b190e2f761471f48
Unknown <10795294@my.uvu.edu>
2018-09-17 15:49:42 -0600

    Scripting practice

---
+++ 1610/Assets/Scripts/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public CharacterController Character;
+	public float Speed;
+	public Vector3 MoveSpeed;
+	public Vector3 RotateSpeed;
+	void Update ()
+	{
+		MoveSpeed.x = Speed * Input.GetAxis("Horizontal");
+		MoveSpeed *= Time.deltaTime;
+		Character.Move(MoveSpeed);
+	}
+}
---
+++ 1610/Assets/Scripts/Variables.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+	public bool IsAWizard;
+
+	public int AmmoCount = 100;
+	public int InventoryCount;
+
+	public float Health = 1.0f;
+	public float Money;
+
+	public string PlayerName;
+	public string Team = "Wolves";
+
+	public Vector3 Location;
+
+	// Use this for initialization
+	void Start () {
+		print(PlayerName);
+		print("has");
+		print("$"+Money);
+
+		if (IsAWizard)
+		{
+			print(PlayerName + " is a Wizard.");
+		}
+		else
+		{
+			print(PlayerName + " is a Muggle.");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = Location;
+	}
+}

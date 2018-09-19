c8ed592a895224972772512bc7c02d4591eb4787
Unknown <10795294@my.uvu.edu>
2018-09-19 15:40:39 -0600

    Created Event

---
+++ 1610/Assets/Scripts/AddValue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class AddValue : ScriptableObject
+{
+	public FloatData ValueObj;
+	public FloatData MaxValue;
+	public FloatData MinValue;
+	
+	public UnityEvent EventMax;
+	public UnityEvent EventMin;
+
+	public void AddValueToObj(FloatData data)
+	{
+		ValueObj.Value += data.Value;
+		
+		if (ValueObj.Value >= MaxValue.Value)
+		{
+			Event.Invoke();
+		}
+
+		if (ValueObj.Value <= MinValue.Value)
+		{
+			Event.Invoke();
+		}
+	}
+}

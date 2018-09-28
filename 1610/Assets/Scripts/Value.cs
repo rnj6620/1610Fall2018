27af067733d7c01a5cf4d91cab7eef3a7316def6
Unknown <10795294@my.uvu.edu>
2018-09-27 19:34:49 -0600

    Creating Value

---
+++ 1610/Assets/Scripts/Value.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Value : MonoBehaviour
+{
+    public FloatData Data;
+    
+    public void AddValueToObject(FloatData data)
+    {
+        Data.Value += data.Value;
+    }
+
+    public void SubtractValueToObject(FloatData data)
+    {
+        Data.Value -= data.Value;
+    }
+
+    public void MultiplyValueToObject(FloatData data)
+    {
+        Data.Value *= data.Value;
+    }
+
+    public void DivideValueToObject(FloatData data)
+    {
+        Data.Value /= data.Value;
+    }
+}

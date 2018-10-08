8f5e9bed90ab91b281eae7e2a6f732516970b8c3
Unknown <10795294@my.uvu.edu>
2018-10-08 15:31:35 -0600

    Made Bullets

---
+++ 1610/Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds(2);
+		Destroy(gameObject);
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+		
+	}
+}

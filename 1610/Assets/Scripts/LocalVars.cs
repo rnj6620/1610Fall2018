eb8dd96ad76c49452214f0d723aba979affaa17d
Unknown <10795294@my.uvu.edu>
2018-10-10 15:31:54 -0600

    Created Variables

---
+++ 1610/Assets/Scripts/LocalVars.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalVars : MonoBehaviour
+{
+
+	string Name;
+
+	public void Call()
+	{
+		Name = "Yuuri";
+		print(Name);
+	}
+
+	public void Call(string newName)
+	{
+		print(newName);
+		print(Name);
+	}
+	
+}
---
+++ 1610/Assets/Scripts/WhatCanSeeIt.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhatCanSeeIt: ScriptableObject
+{
+
+    private float health;
+    protected float PowerLevel;
+    public float EnergyLevel;
+    
+    private void AddHealth()
+    {
+        health += 100;
+    }
+
+   
+    protected void AddPower()
+    {
+        AddHealth();
+    }
+    
+    public void AddEnergy()
+    {
+    
+    }
+}
+
+public class ICanSeeIt: MonoBehaviour
+{
+    private WhatCanSeeIt newObject;
+
+    private void Start()
+    {
+        newObject.EnergyLevel = 100;
+        newObject.AddEnergy();
+    }
+}
+
+public class Child : WhatCanSeeIt
+{
+    void CallThings()
+    {
+        PowerLevel = 200;
+        EnergyLevel = 300;
+        AddPower();
+        AddEnergy();
+    }
+}

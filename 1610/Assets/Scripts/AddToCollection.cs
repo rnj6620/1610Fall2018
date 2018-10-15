2be08ef7c7b2b4c37ecbea6ed427c96171685c9b
Unknown <10795294@my.uvu.edu>
2018-10-15 15:36:50 -0600

    Add Collections

---
+++ 1610/Assets/Scripts/AddToCollection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AddToCollection : ScriptableObject
+{
+	public ObjectCollection Collection;
+	
+	public void Call(ScriptableObject obj)
+	{
+		Collection.ObjectList.Add(obj);
+	}
+}

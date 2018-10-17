f360f1546cad6df57579db52dc7dfebb6fe784ed
Unknown <10795294@my.uvu.edu>
2018-10-17 15:41:06 -0600

    Added Health Bar

---
+++ 1610/Assets/Scripts/UpdateImageValue.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateImageValue : MonoBehaviour
+{
+	public FloatData Data;
+	private Image image;
+
+	// Use this for initialization
+	void Start ()
+	{
+		image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		image.fillAmount = Data.Value;
+	}
+}

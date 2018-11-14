3a379f3294f3a653b638fb2f22b1d415977b7fbd
Unknown <10795294@my.uvu.edu>
2018-11-14 14:29:05 -0700

    Created enemies

---
+++ 1610/Assets/Scripts/StateMachine.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class StateMachine : ScriptableObject {
+
+	public GameStates.States GameState;
+	public UnityEvent Starting, Loading, Playing, Dying, Ending;
+	
+	// Update is called once per frame
+	public void Run () {
+		
+		switch (GameState)
+		{
+			case GameStates.States.Starting:
+				Starting.Invoke();
+				break;
+			
+			case GameStates.States.Loading:
+				Loading.Invoke();
+				break;
+			
+			case GameStates.States.Playing:
+				Playing.Invoke();
+				break;
+			case GameStates.States.Dying:
+				Dying.Invoke();
+				break;
+			
+			case GameStates.States.Ending:
+				Ending.Invoke();
+				break;
+		}
+	}
+}

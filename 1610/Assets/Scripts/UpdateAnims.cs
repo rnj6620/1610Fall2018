e8f8d6b01e0228b960a7edcc8c7a01a860941605
Unknown <10795294@my.uvu.edu>
2018-11-30 10:22:33 -0700

    created animation

---
+++ 1610/Assets/Scripts/UpdateAnims.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateAnims : MonoBehaviour {
+
+    private Animator anims;
+    public float speed;
+    
+	// Use this for initialization
+	void Start () {
+		anims = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anims.SetFloat("Speed", speed);
+	}
+}
